a888db47a6e92bbbe7f608a3c8023660a5433957
github username <github email address>
2020-01-21 11:06:40 -0500

    Homepage successfully renders people data. Style and refactoring needed.

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -9,11 +9,36 @@ class HomePage extends React.Component {
     }
   }
 
+  renderPeople = () => {
+    let { data } = this.props.people
+    data.map(({ first_name, last_name, email_address, title }) => {
+      return (
+        <li>
+          <p>Name: `${first_name} ${last_name}`</p>
+          <p>Title: `${title}`</p>
+          <p>Email: `${email_address}`</p>
+        </li>
+      )
+    })
+  }
+
   render() {
     return (
       <div>
         <h1>Home Page</h1>
-        {this.props.people.data && <p>People data...</p>}
+        {this.props.people.data && (
+          <ul>
+            {this.props.people.data.map(({ first_name, last_name, email_address, title }) => {
+              return (
+                <li>
+                  <p>Name: {first_name} {last_name}</p>
+                  <p>Title: {title}</p>
+                  <p>Email: {email_address}</p>
+                </li>
+              )
+            })}
+          </ul>
+        )}
       </div>
     )
   }

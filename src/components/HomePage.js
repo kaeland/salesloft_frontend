f3d4688867dd5445cda16fa9c48310ab759c9f6e
github username <github email address>
2020-01-21 12:54:11 -0500

    Homepage Successfully toggles showing the frequency of characters in each person's email address.

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,8 +1,12 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { startSetPeople } from '../redux/actions/people'
+import { sortEmailByLetterFrequency } from '../utils/utils'
+import Table from './Table'
 
 class HomePage extends React.Component {
+  state = { showFrequency: false }
+
   componentDidMount() {
     if (localStorage.getItem("jwt") !== null) {
       this.props.startSetPeople()
@@ -14,18 +18,33 @@ class HomePage extends React.Component {
     return data.map(({ first_name, last_name, email_address, title }) => {
       return (
         <li>
-          <p>Name: {first_name} {last_name}</p>
-          <p>Title: {title}</p>
-          <p>Email: {email_address}</p>
+          {this.state.showFrequency === true
+            ? this.renderTable(email_address)
+            : (
+              <div>
+                <p>Name: {first_name} {last_name}</p>
+                <p>Title: {title}</p>
+                <p>Email: {email_address}</p>
+              </div>
+            )
+          }
         </li>
       )
     })
+  }
+
+  renderTable = (email) => {
+    const frequency = sortEmailByLetterFrequency(email)
+    return <Table frequency={frequency} />
   }
 
   render() {
     return (
       <div>
         <h1>Home Page</h1>
+        <button onClick={() => this.setState({ showFrequency: !this.state.showFrequency })}>
+          Toggle Frequency
+        </button>
         {this.props.people.data && (
           <ul>
             {this.renderPeople()}

04b7c753c632cb6c3e74ecc8635f8f6283523636
github username <github email address>
2020-01-22 06:11:06 -0500

    Added route for Signup component to App component.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import HomePage from './components/HomePage'
 import LoginPage from './components/LoginPage'
+import SignupPage from './components/SignupPage'
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Switch>
         <Route path="/" exact component={HomePage} />
         <Route path="/login" component={LoginPage} />
+        <Route path="/signup" component={SignupPage} />
       </Switch>
     </Router>
   );

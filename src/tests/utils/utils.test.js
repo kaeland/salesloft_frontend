9b0a2b983aaa8a7e55b7a699518ed1e188a6ab17
github username <github email address>
2020-01-24 04:58:18 -0500

    Added context to tests using  and  functions.

--- src/tests/utils/utils.test.js
+++ src/tests/utils/utils.test.js
@@ -13,48 +13,56 @@ import {
   arrayOfSimilarEmails,
   parsedEmail, 
   parsedEmailToArray
-} from '../fixtures/peopleData'
+} from '../fixtures/peopleData' 
 
-test('should map an email to an object', () => {
-  let obj = mapEmailToHash(exampleEmail)
-  expect(obj).toEqual(parsedEmail)
+describe('mapEmailToHash', () => {
+  it('should map an email to an object', () => {
+    let obj = mapEmailToHash(exampleEmail)
+    expect(obj).toEqual(parsedEmail)
+  })
 })
 
-test('should map an email to an array', () => {
-  let array = mapEmailHashToArray(exampleEmail)
-  expect(array).toEqual(parsedEmailToArray)
+describe('mapEmailHashToArray', () => {
+  it('should map an email to an array', () => {
+    let array = mapEmailHashToArray(exampleEmail)
+    expect(array).toEqual(parsedEmailToArray)
+  })
 })
 
-test('should sort email by frequency count', () => {
-  let array = sortEmailByLetterFrequency(exampleEmail)
-  const sorter = (a, b) => b[1] - a[1]
-  expect(parsedEmailToArray.sort(sorter)).toEqual(array)
+describe('sortEmailByLetterFrequency', () => {
+  it('should sort email by frequency count', () => {
+    let array = sortEmailByLetterFrequency(exampleEmail)
+    const sorter = (a, b) => b[1] - a[1]
+    expect(parsedEmailToArray.sort(sorter)).toEqual(array)
+  })
 })
 
-test('should return the number of characters likely to be between strings', () => {
-  let distance = editDistance(arrayOfSimilarEmails[0], arrayOfSimilarEmails[1])
-  expect(distance).toBe(2)
+describe('editDistance', () => {
+  it('should return the number of characters likely to be between strings', () => {
+    let distance = editDistance(arrayOfSimilarEmails[0], arrayOfSimilarEmails[1])
+    expect(distance).toBe(2)
+  })
 })
 
-test('should return a number', () => {
-  let percentage = similarity(arrayOfSimilarEmails[0], arrayOfSimilarEmails[1])
-  expect(typeof percentage).toBe('number')
+describe('similarity', () => {
+  it('should return a number', () => {
+    let percentage = similarity(arrayOfSimilarEmails[0], arrayOfSimilarEmails[1])
+    expect(typeof percentage).toBe('number')
+  })
+  
+  it('should return a number less than 1, but greater than 0', () => {
+    let percentage = similarity(arrayOfSimilarEmails[0], arrayOfSimilarEmails[1])
+    expect(percentage).toBeLessThan(1)
+    expect(percentage).toBeGreaterThan(0)
+  })
+  
+  it('should return a reasonably high similarity percentage for similar strings', () => {
+    let percentage = similarity(arrayOfSimilarEmails[0], arrayOfSimilarEmails[1])
+    expect(percentage).toBeCloseTo(0.95, 1)
+  })
+
+  it('should return a reasonably low similarity percentage for dissimilar strings', () => {
+    let percentage = similarity('michael.jackson@example.com', 'jill.scott@music.com')
+    expect(percentage).toBeLessThan(0.85)
+  })
 })
-
-test('should return a number less than 1, but greater than 0', () => {
-  let percentage = similarity(arrayOfSimilarEmails[0], arrayOfSimilarEmails[1])
-  expect(percentage).toBeLessThan(1)
-})
-
-test('should return a number greater than 0',  () => {
-  let percentage = similarity(arrayOfSimilarEmails[0], arrayOfSimilarEmails[1])
-  expect(percentage).toBeGreaterThan(0)
-})
-
-test('should return a reasonable similarity percentage for similar strings', () => {
-  let percentage = similarity(arrayOfSimilarEmails[0], arrayOfSimilarEmails[1])
-  expect(percentage).toBeCloseTo(0.9, 1)
-})
-
-
-
